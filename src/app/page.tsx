98b6db8982b28be90a0cba8013756c3056363ca4
R1shabh-Gupta <imailrishabhgupta@gmail.com>
2024-06-20 12:45:17 +0530

    ui: Added hero section with call to action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,76 @@
-import { Button } from "@/components/ui/button";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import SquigglyLines from "@/components/SquigglyLines";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <Button>Click me</Button>;
+  return (
+    <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
+      <Head>
+        <title>Photo Restorer</title>
+      </Head>
+      <Header />
+      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-20">
+        <a
+          href="https://github.com/R1shabh-Gupta/Unfade"
+          target="_blank"
+          rel="noreferrer"
+          className="border rounded-2xl py-1 px-4 text-slate-500 text-sm mb-5 hover:scale-105 transition duration-300 ease-in-out"
+        >
+          Give us a star on <span className="font-semibold">GitHub</span>!
+        </a>
+
+        <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-slate-900 sm:text-7xl">
+          Restoring old photos{" "}
+          <span className="relative whitespace-nowrap text-[#3290EE]">
+            <SquigglyLines />
+            <span className="relative">using AI</span>
+          </span>{" "}
+          for everyone.
+        </h1>
+
+        <p className="mx-auto mt-12 max-w-xl text-lg text-slate-700 leading-7">
+          Have old and blurry face photos? Let our AI restore them so those
+          memories can live on. 100% free – restore your photos today.
+        </p>
+
+        <Link
+          className="bg-black rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-black/80"
+          href="/restore"
+        >
+          Restore your photos
+        </Link>
+
+        <div className="flex justify-between items-center w-full flex-col sm:mt-10 mt-6">
+          <div className="flex flex-col space-y-10 mt-4 mb-16">
+            <div className="flex sm:space-x-2 sm:flex-row flex-col">
+              <div>
+                <h2 className="mb-1 font-medium text-lg">Original Photo</h2>
+                <Image
+                  alt="Original photo of a house"
+                  src="/imageToBeRestored.webp"
+                  className="w-96 h-96 rounded-2xl"
+                  width={400}
+                  height={400}
+                />
+              </div>
+              <div className="sm:mt-0 mt-8">
+                <h2 className="mb-1 font-medium text-lg">Restored Photo</h2>
+                <Image
+                  alt="Restored photo of the house"
+                  width={400}
+                  height={400}
+                  src="/restoredImage.webp"
+                  className="w-96 h-96 rounded-2xl sm:mt-0 mt-2"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
 }
